9aa6b07ec8873c96fcede41b37bc4c0e7970a5cf
Chris Birdsong <34635830+smtpman@users.noreply.github.com>
2022-04-18 11:37:09 -0500

    Add files via upload

---
+++ install_choco_packages.ps1
@@ -0,0 +1,10 @@
+choco install googlechrome -y
+choco install spotify -y
+choco install vlc -y
+choco install 7zip -y
+choco install discord -y
+choco install git -y
+choco install vscode -y
+choco install tailscale -y
+choco install powertoys -y
+choco install Microsoft-Hyper-V -All --source windowsfeatures
